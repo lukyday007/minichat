d06234e98e498012b528036736f271d8abb32208
dayul <rainbow9517@naver.com>
2025-10-12 18:28:24 +0900

    feat: 비동기 작업 처리를 위한 커스텀 스레드풀 설정 추가

--- src/main/java/com/dy/minichat/config/ThreadPoolConfig.java
+++ src/main/java/com/dy/minichat/config/ThreadPoolConfig.java
@@ -1,4 +1,28 @@
 package com.dy.minichat.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
 public class ThreadPoolConfig {
+
+    @Bean(name = "customThreadPool")
+    public Executor customThreadPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Custom-Async-");
+
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        executor.initialize();
+        return executor;
+    }
 }
