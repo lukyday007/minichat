c4f437d267b2d6ae58eef6733836e5ba5ed2b558
dayul <rainbow9517@naver.com>
2025-10-13 15:13:22 +0900

    refactor: 생성자 주입 리팩토링

--- src/main/java/com/dy/minichat/service/UserService.java
+++ src/main/java/com/dy/minichat/service/UserService.java
@@ -1,14 +1,8 @@
 package com.dy.minichat.service;
 
-import com.dy.minichat.config.id.ChatIdGenerator;
-import com.dy.minichat.config.id.MessageIdGenerator;
-import com.dy.minichat.config.id.UserChatIdGenerator;
 import com.dy.minichat.config.id.UserIdGenerator;
 import com.dy.minichat.dto.request.UserRequestDTO;
 import com.dy.minichat.entity.User;
-import com.dy.minichat.repository.ChatRepository;
-import com.dy.minichat.repository.MessageRepository;
-import com.dy.minichat.repository.UserChatRepository;
 import com.dy.minichat.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,16 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final ChatRepository chatRepository;
-    private final MessageRepository messageRepository;
     private final UserRepository userRepository;
-    private final UserChatRepository userChatRepository;
-
-
     private final UserIdGenerator userIdGenerator;
-    private final ChatIdGenerator chatIdGenerator;
-    private final MessageIdGenerator messageIdGenerator;
-    private final UserChatIdGenerator userChatIdGenerator;
 
     // == 유저 API == //
     @Transactional
