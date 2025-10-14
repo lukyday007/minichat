0d72b1baf6e96bcb29f045a17327d156ceb10e79
dayul <rainbow9517@naver.com>
2025-10-14 17:27:49 +0900

    feat: 로컬 테스트용 어노테이션 추가

--- src/test/java/com/dy/minichat/MinichatApplicationTests.java
+++ src/test/java/com/dy/minichat/MinichatApplicationTests.java
@@ -2,8 +2,13 @@ package com.dy.minichat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // <-- 이 부분을 수정!
+@ActiveProfiles("local1")
+// 테스트 시 gRPC 서버는 비활성화하여 포트 충돌을 원천 방지
+@TestPropertySource(properties = "grpc.server.port=-1")
 class MinichatApplicationTests {
 
 	@Test
