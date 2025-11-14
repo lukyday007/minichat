4ce596a5cf43be0258930d85d4b4e7c7075c5753
dayul <rainbow9517@naver.com>
2025-11-15 03:14:32 +0900

    feat : db replica 구현

--- src/main/java/com/dy/minichat/component/RoutingDataSource.java
+++ src/main/java/com/dy/minichat/component/RoutingDataSource.java
@@ -1,4 +1,40 @@
 package com.dy.minichat.component;
 
-public class RoutingDataSource {
+import com.dy.minichat.property.DataSourceProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class RoutingDataSource extends AbstractRoutingDataSource {
+    private final MasterReplicaKeyRouter masterReplicaKeyRouter;
+
+    public RoutingDataSource(Map<Object, Object> dataSourceMap, MasterReplicaKeyRouter masterReplicaKeyRouter) {
+        // System.out.println("-------------->> RoutingDataSource RoutingDataSource");
+        this.masterReplicaKeyRouter = masterReplicaKeyRouter;
+        super.setTargetDataSources(dataSourceMap);
+        this.afterPropertiesSet();
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        Object key;
+        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            // System.out.println("[SLAVE TX]");
+            key = masterReplicaKeyRouter.getReplicaKey();
+            //System.out.println("     └─ determineCurrentLookupKey → " + key);
+        } else {
+            // System.out.println("[MASTER TX]");
+            key= masterReplicaKeyRouter.getMasterKey();
+            // System.out.println("     └─ determineCurrentLookupKey → " + key);
+        }
+        // System.out.println("DATASOURCE: " + key);
+        return key;
+    }
 }
--- src/main/java/com/dy/minichat/config/DataSourceConfig.java
+++ src/main/java/com/dy/minichat/config/DataSourceConfig.java
@@ -1,25 +1,64 @@
 package com.dy.minichat.config;
 
+import com.dy.minichat.config.replica.MasterReplicaKeyRouter;
+import com.dy.minichat.config.replica.RoutingDataSource;
+import com.dy.minichat.property.DataSourceProperty;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
+@RequiredArgsConstructor
 @Configuration
 public class DataSourceConfig {
-    @Bean
-    public DataSource dataSource() {
+
+    private final DataSourceProperty dataSourceProperty;
+
+    // 로그 찍기용 createDataSource
+    public DataSource createDataSource(String key, String url, String username, String password) {
+        System.out.println("     └─ createReplicaDataSourceConfig [" + key + "]");
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3301/minichatdb?characterEncoding=UTF-8&serverTimezone=Asia/Seoul");
-        config.setUsername("root");
-        config.setPassword("password");
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
         config.setMaximumPoolSize(20);
         config.setMinimumIdle(20);
-        config.addDataSourceProperty("characterEncoding", "UTF-8");
-
         return new HikariDataSource(config);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        // System.out.println("---> ReplicaDataSourceConfig dataSource()");
+
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+        MasterReplicaKeyRouter masterReplicaKeyRouter = new MasterReplicaKeyRouter();
+
+        DataSource masterDataSource = createDataSource(
+                dataSourceProperty.getMaster().getKey(),
+                dataSourceProperty.getMaster().getUrl(),
+                dataSourceProperty.getUsername(),
+                dataSourceProperty.getPassword());
+        dataSourceMap.put(dataSourceProperty.getMaster().getKey(), masterDataSource);
+        masterReplicaKeyRouter.setMasterKey(dataSourceProperty.getMaster().getKey());
+
+        dataSourceProperty.getReplicas().forEach(databaseInfo -> {
+            DataSource replicaDataSource = createDataSource(
+                    databaseInfo.getKey(),
+                    databaseInfo.getUrl(),
+                    dataSourceProperty.getUsername(),
+                    dataSourceProperty.getPassword());
+            dataSourceMap.put(databaseInfo.getKey(), replicaDataSource);
+            masterReplicaKeyRouter.addReplicaKey(databaseInfo.getKey());
+        });
+
+        // return new RoutingDataSource(dataSourceMap, masterSlaveKeyRouter);
+        return new LazyConnectionDataSourceProxy(new RoutingDataSource(dataSourceMap, masterReplicaKeyRouter));
     }
 }
