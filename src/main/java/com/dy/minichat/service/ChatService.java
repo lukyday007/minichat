559b9941545135b318779861a5a37e3eed7bd901
dayul <rainbow9517@naver.com>
2025-11-15 03:07:56 +0900

    fix: redis template 수정

--- src/main/java/com/dy/minichat/service/ChatService.java
+++ src/main/java/com/dy/minichat/service/ChatService.java
@@ -20,10 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
