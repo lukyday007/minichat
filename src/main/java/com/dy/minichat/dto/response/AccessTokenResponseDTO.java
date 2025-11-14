2e649ad1ce57923e8e376c224a82ad8dd4ebc298
dayul <rainbow9517@naver.com>
2025-11-15 03:02:40 +0900

    feat: 로그인 및 순수 jwt 인증 구현

---
+++ src/main/java/com/dy/minichat/dto/response/AccessTokenResponseDTO.java
@@ -0,0 +1,10 @@
+package com.dy.minichat.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AccessTokenResponseDTO {
+    private String accessToken;
+}
